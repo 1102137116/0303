585c8297887b4bcb10ba9753530e83ce00dc6329
1102137116 <1102137116@gm.kuas.edu.tw>
2016-03-03 15:00:49 +0800

    add Hello cs

---
+++ WebApplication1/Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Hello
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Hello");
+            System.Console.ReadLine();
+        }
+    }
+}
